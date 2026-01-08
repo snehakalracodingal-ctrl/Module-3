5d8c9be9d0b78f31afa07f67eea15c3a5eb6655a
snehakalracodingal-ctrl <snehakalra.codingal@gmail.com>
2026-01-08 19:43:16 +0530

    lesson5

---
+++ ACP5.py
@@ -0,0 +1,9 @@
+import math
+
+angle = float(input("Enter angle in degrees: "))
+
+radian = math.radians(angle)
+
+print("Sin value:", math.sin(radian))
+print("Cos value:", math.cos(radian))
+print("Tan value:", math.tan(radian))
